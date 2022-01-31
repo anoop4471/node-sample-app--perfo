e79d7fb002999f25c76af68560f2307e5dcf7737
anoop4471 <94365521+anoop4471@users.noreply.github.com>
2022-01-31 10:16:00 +0530

    Update az.sh

--- TF Devops - azure app service/az.sh
+++ TF Devops - azure app service/az.sh
@@ -1,3 +1,3 @@
 az group create -l eastus2 -n terraform-state
 az storage account create -g terraform-state -l eastus2 --name storagetrain97651 --sku Standard_LRS --encryption-services blob
-az storage container create --name terraform-state-container --account-name storagetrain56789
+az storage container create --name terraform-state-container --account-name storagetrain97651
